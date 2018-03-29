d59a24f30ac137ceef35751a7fdac505672e21f8
Alexander Jackson <A.Jackson.6@warwick.ac.uk>
2018-03-29 16:34:44 +0100

    Added comments to the IMazeGenerator interface.

--- IMazeGenerator.java
+++ IMazeGenerator.java
@@ -1,3 +1,10 @@
+/**
+ * Defines an interface for Maze Generators, specifying the methods they must implement
+ */
 public interface IMazeGenerator {
+    /**
+     * Defines that Maze Generators must have a GenerateMaze method
+     * @return Returns a maze that the generator generated
+     */
     public Maze GenerateMaze();
 }
