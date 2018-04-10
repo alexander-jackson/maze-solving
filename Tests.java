34a1f73fef3c64033241e41f3fb9668f4d8aca99
Alexander Jackson <A.Jackson.6@warwick.ac.uk>
2018-04-10 18:14:33 +0100

    Updated the Tests file to test the new A* Algorithm solving.

--- Tests.java
+++ Tests.java
@@ -1,10 +1,17 @@
 public class Tests {
 
-    static final int Width = 10;
-    static final int Height = 10;
+    static final int Width = 70;
+    static final int Height = 70;
 
     public static void main(String[] args) {
-        Maze m = new Maze(Width, Height);
+        DepthFirstMazeGenerator DFMG = new DepthFirstMazeGenerator(Width, Height);
+        Maze m = DFMG.GenerateMaze();
         m.PrettyDisplay();
+
+        AStarMazeSolver ASMS = new AStarMazeSolver(m);
+        Coordinate Start = new Coordinate(1, 1);
+        Coordinate End = new Coordinate(m.GetWidth() - 2, m.GetHeight() - 2);
+        Maze Solved = ASMS.SolveMaze(Start, End);
+        Solved.PrettyDisplay();
     }
 }
